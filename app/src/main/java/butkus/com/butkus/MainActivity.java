466d01a88b9b411a9b2d2d6fcb8f3a89ec54168e
kitty jutbring <kittyj@cmail.nu>
2015-12-14 19:54:14 +0100

    add "flippiga" colors

--- app/src/main/java/butkus/com/butkus/MainActivity.java
+++ app/src/main/java/butkus/com/butkus/MainActivity.java
@@ -1,10 +1,18 @@
 package butkus.com.butkus;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ShareActionProvider;
 import android.widget.TextView;
 
 import java.io.IOException;
@@ -12,6 +20,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Random;
 import java.util.concurrent.ExecutionException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -20,18 +29,34 @@ import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
+    String quote;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.butkusButton);
+        button.setBackgroundColor(Color.parseColor(randomColor()));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+
+        return true;
     }
 
     public void butkusClicked(View view) {
-        String quote = getButkus();
+        quote = getButkus();
 
         TextView quoteTextView = (TextView) findViewById(R.id.quoteTextView);
+        Button button = (Button) findViewById(R.id.butkusButton);
 
+        quoteTextView.setTextColor(Color.parseColor(randomColor()));
         quoteTextView.setText(quote);
+        button.setBackgroundColor(Color.parseColor(randomColor()));
     }
 
     public String getButkus() {
@@ -60,6 +85,14 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         return quote;
+    }
+
+    public void shareIntent(String quote) {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, quote);
+        sendIntent.setType("text/plain");
+        startActivity(sendIntent);
     }
 
     public class DownloadButkusTask extends AsyncTask<String, Void, String> {
@@ -96,6 +129,13 @@ public class MainActivity extends AppCompatActivity {
             }
             return null;
         }
+    }
+
+    public String randomColor() {
+        String[] colors = new String[]{"#af8700", "#d75f00", "#d70000", "#af005f", "#5f5faf",
+                                        "#0087ff", "#00afaf", "#5f8700"};
+        int random = new Random().nextInt(colors.length);
+        return colors[random];
     }
 
 }
